f8172377128669bfea05e762a53bdb1190e8258c
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2021-12-12 13:05:16 -0800

    makes api call and grabs all punk data through useEffect

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import { useState, useEffect } from "react";
 import styled from "styled-components";
 import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
+import axios from "axios";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+
+  useEffect(() => {
+    const getNFTs = async () => {
+      const openSeaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0x87c5339DD9fcF1307A1A942aE47C94d2026C72f3&order_direction=asc"
+      );
+      console.log(openSeaData.data.assets);
+      setPunkListData(openSeaData.data.assets);
+    };
+    return getNFTs();
+  }, []);
+
   return (
     <Container>
       <Header />
