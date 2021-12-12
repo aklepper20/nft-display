6aa142dcaff7a9ca0812261b4566044e4860b4eb
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2021-12-12 12:05:23 -0800

    completes header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import punkLogo from "../assets/cryptopunk-logo.png";
 import searchIcon from "../assets/search.png";
+import themeSwitchIcon from "../assets/theme-switch.png";
 
 function Header() {
   return (
@@ -15,6 +16,17 @@ function Header() {
         </SearchIcon>
         <SearchInput type="text" placeholder="Collection, item or user..." />
       </SearchBar>
+      <HeaderContent>
+        <p>Drops</p>
+        <p>Marketplace</p>
+        <p>Create</p>
+      </HeaderContent>
+      <HeaderActions>
+        <ThemeSwitchContainer>
+          <img src={themeSwitchIcon} alt="Theme Switch Icon" />
+        </ThemeSwitchContainer>
+      </HeaderActions>
+      <LoginButton>GET IN</LoginButton>
     </Container>
   );
 }
@@ -52,5 +64,45 @@ const SearchInput = styled.input`
   border: none;
   width: 100%;
   outline: none;
+  color: #eee;
+  font-size: 16px;
+`;
+
+const HeaderContent = styled.div`
+  display: flex;
+  color: #a1a5b0;
+  p {
+    margin: 10px;
+  }
+`;
+
+const HeaderActions = styled.div`
+  display: flex;
+  align-items: center;
+
+  div {
+    margin: 10px;
+  }
+`;
+
+const ThemeSwitchContainer = styled.div`
+  display: flex;
+  background-color: #1c1c1e;
+  border-radius: 50px;
+  padding: 15px;
+  justify-content: center;
+  align-items: center;
+  object-fit: contain;
+
+  img {
+    height: 25px;
+  }
+`;
+
+const LoginButton = styled.div`
+  background: linear-gradient(to right, #59f9b7, #66feea);
+  padding: 15px 40px;
+  border-radius: 50px;
+  color: black;
 `;
 export default Header;
